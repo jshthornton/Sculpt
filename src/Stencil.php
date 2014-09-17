5e023693ec64ec07bfe5ef6164bf8d7105d76ee9
Joshua Thornton <joshua.thornton@readingroom.com>
2014-09-17 13:35:49 +0800

    Base code.

---
+++ src/Stencil.php
@@ -0,0 +1,22 @@
+<?php
+ 
+class Stencil {
+	public static $EXTENSION = '.tpl.php';
+
+	public static function parse($__path, $__data = array()) {
+		return self::render($__path, $__data, false);
+	}
+
+	public static function render($__path, $__data = array(), $__echo = true) {
+		ob_start();
+		extract($__data);
+		require $__path . self::$EXTENSION;
+ 
+		$__out = ob_get_clean();
+		if($__echo === true) {
+			echo $__out;
+		} else {
+			return $__out;
+		}
+	}
+}
